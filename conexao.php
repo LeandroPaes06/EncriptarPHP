8a114c7d607cf22e556491e0d4e0965d9e0a6e87
Gab <127546521+LeandroPaes06@users.noreply.github.com>
2023-04-01 00:05:26 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php
+
+$ servidor = " localhost ";
+$ usuario = " root ";
+$ senha = "";
+$ dbname = " crip ";
+
+
+//criar conexão com o DB
+$ conn = mysqli_connect( $ servidor , $ usuario , $ senha , $ dbname );
+
+?>
